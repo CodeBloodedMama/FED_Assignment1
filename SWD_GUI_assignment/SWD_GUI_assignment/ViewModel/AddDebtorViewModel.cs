719d683379174bfce1388b8d31eec10c1b9b7f99
Marcin Szymanek <202009418@post.au.dk>
2022-10-05 12:50:21 +0200

    Added swd assignment init repo

---
+++ SWD_GUI_assignment/SWD_GUI_assignment/ViewModel/AddDebtorViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SWD_GUI_assignment.Annotations;
+using SWD_GUI_assignment.Model;
+
+namespace SWD_GUI_assignment.ViewModel
+{
+    public class AddDebtorViewModel: INotifyPropertyChanged
+    {
+        private AccountCollection _accs;
+
+        public AddDebtorViewModel(ref AccountCollection accs)
+        {
+            _accs = accs;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        AccountModel AccountModel=new AccountModel(null);
+
+        public String Name
+        {
+            get => AccountModel.Name;
+            set
+            {
+                if (value != AccountModel.Name)
+                {
+                    AccountModel.Name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public double Balance
+        {
+            get => AccountModel.Balance;
+            set
+            {
+                if (value != AccountModel.Balance)
+                {
+                    AccountModel.ChangeBalance(value);
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public void AddDebter_OnClick()
+        {
+            if (AccountModel != null)
+            {
+                _accs.AddNewDebter(AccountModel);
+            }
+        }
+
+        public void CloseApplication_OnClick()
+        {
+            //this.Close;
+        }
+
+
+        private ICommand _AddDebtorCommand;
+
+        public ICommand AddDebtorCommand
+        {
+        
+            get
+            {
+                return _AddDebtorCommand ?? (_AddDebtorCommand = new RelayCommand(AddDebter_OnClick));
+            }
+        
+        }
+
+        private ICommand _CloseCommand;
+
+        public ICommand CloseCommand
+        {
+
+            get
+            {
+                return _CloseCommand ?? (_CloseCommand = new RelayCommand(CloseApplication_OnClick));
+            }
+
+        }
+
+
+    }
+
+} 
+
+
---
+++ SWD_GUI_assignment/SWD_GUI_assignment/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using SWD_GUI_assignment.Model;
+
+namespace SWD_GUI_assignment.ViewModel
+{
+	public class MainWindowViewModel: INotifyPropertyChanged
+	{
+        public AccountCollection accounts = new AccountCollection();
+
+
+        public MainWindowViewModel()
+        {
+         CurrentDebtor = accounts.Debtors[0];
+        }
+
+       public AccountCollection Accounts
+       {
+           get { return accounts; }
+       }
+
+       
+
+        AccountModel currentDebtor = null;
+        public AccountModel CurrentDebtor
+        {
+            get { return currentDebtor; }
+            set
+            {
+                if (currentDebtor != value)
+                {
+                    currentDebtor = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        // On Proberty Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //Commands
+
+        private ICommand _OpenAddDebterCommand;
+
+        public ICommand OpenAddDebterCommand
+        {
+            get
+            {
+                return _OpenAddDebterCommand ?? (_OpenAddDebterCommand = new RelayCommand(()=> {
+                    var vm = new AddDebtorViewModel(ref accounts);
+                    
+                    var addDebtorWin = new AddDebtor()
+                        {
+                            DataContext = vm
+                        };
+
+                    addDebtorWin.Show();
+                }));
+            }
+        }
+
+        private ICommand _OpenOverviewCommand;
+        public ICommand OpenOverviewCommand
+        {
+            get
+            {
+                return _OpenOverviewCommand ?? (_OpenOverviewCommand = new RelayCommand(()=>
+                {
+                    var vm = new DebtorOverviewViewModel(ref currentDebtor);
+
+                    DebtorOverview overViewWin = new DebtorOverview(CurrentDebtor)
+                        {
+                            DataContext = vm 
+                        };
+                    overViewWin.Show();
+                   
+                }));
+            }
+        }
+
+    }
+}
