e8826d204eed11009f04c61a1c6b3ce958848158
CodeBloodedMama <84911345+CodeBloodedMama@users.noreply.github.com>
2022-10-06 16:05:35 +0200

    Update DebtbookModel.cs
    Deleted a class not being used

--- The Debt Book/The Debt Book/Model/DebtbookModel.cs
+++ The Debt Book/The Debt Book/Model/DebtbookModel.cs
@@ -1,19 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Prism.Commands;
-using SWD_GUI_assignment.ViewModel;
-
-namespace SWD_GUI_assignment.Model
-{
-    class DebtbookModel
-    {
-        
-    }
-} 
 
 
